5184fb98e07291c959eb5f6ec1aa92d91109e106
Tomek Kopczuk <tkopczuk@gmail.com>
2018-10-08 14:12:51 +0200

    Added CORS.

--- src/app.configuration.ts
+++ src/app.configuration.ts
@@ -2,6 +2,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { InstanceNotFoundExceptionFilter } from './common/filters/entity-not-found-exception.filter';
 
 export function configureApplicaton(app: INestApplication): void {
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalFilters(
